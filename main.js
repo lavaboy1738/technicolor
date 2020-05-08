a4b9c1e7d4a0581709b5a17c89082b4c805d46bc
lavaboy1738 <yanzfeng@bu.edu>
2020-05-08 11:45:20 -0400

    adding randomize button, fixing minor errors with width

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const App = (()=>{
     const blocks = document.querySelectorAll(".block");
     const resetBtn = document.querySelector(".resetBtn");
+    const randomBtn = document.querySelector(".randomBtn");
 
     const setRandomColor = (e) =>{
         const r = Math.floor(Math.random()*256)
@@ -15,12 +16,22 @@ const App = (()=>{
         });
     }
 
+    const randomize = () =>{
+        blocks.forEach((block)=>{
+            const r = Math.floor(Math.random()*256)
+            const g = Math.floor(Math.random()*256)
+            const b = Math.floor(Math.random()*256)
+            block.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+        })
+    }
+
     const listeners = () =>{
         blocks.forEach((element) =>{
             element.addEventListener("mouseover", setRandomColor)
         })
 
         resetBtn.addEventListener("click", resetAll)
+        randomBtn.addEventListener("click", randomize)
     }
 
     return{
